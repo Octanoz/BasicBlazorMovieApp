9b7d71040f6558ea7ecc9c84da3846d2e7f00649
Octanoz <130052848+Octanoz@users.noreply.github.com>
2024-01-04 22:40:27 +0100

    Add project files.

---
+++ Models/MovieModel.cs
@@ -0,0 +1,10 @@
+﻿namespace BasicBlazorMovieApp.Models;
+
+public class MovieModel(int id, string title, string description, string imageUrl, string review)
+{
+    public int Id { get; } = id;
+    public string Title { get; } = title;
+    public string Description { get; } = description;
+    public string ImageUrl { get; } = imageUrl;
+    public string Review { get; } = review;
+}
